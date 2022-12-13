6a64fc12474caeb41a08d97730a8802371d43360
AvresCode <servas230@gmail.com>
2022-12-13 10:46:33 +0100

    Added signup and login fields

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 export const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="signup-container">
+      <h2> Login</h2>
+      <form>
+        <div>
+          {" "}
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          {" "}
+          <input
+            type="text"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          ></input>
+        </div>
+        <br />
+        <button type="submit"> Login</button>
+      </form>
+      <div>
+        {" "}
+        Don't have an account yet? Click <Link to="/signup">here</Link> to sign
+        up
+      </div>
+    </div>
+  );
 };
