956bfd0361c90c6a4c7761d42fe243e616ec7f4b
AvresCode <servas230@gmail.com>
2022-12-15 22:01:53 +0100

    Added onAuthChange to the app

--- src/App.js
+++ src/App.js
@@ -5,8 +5,31 @@ import { SignUp } from "./pages/SignUp";
 import { Login } from "./pages/Login";
 import { Navbar } from "./components/Navbar";
 import { UserProfile } from "./pages/UserProfile";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
+import { useDispatch } from "react-redux";
+import { setUser, clearUserData } from "./store/user/slice";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      // dispatch(setUser({ user, token }));
+      console.log("user", currentUser);
+      if (currentUser) {
+        dispatch(
+          setUser({ user: currentUser, token: currentUser.accessToken })
+        );
+      } else {
+        dispatch(clearUserData());
+      }
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Navbar />
